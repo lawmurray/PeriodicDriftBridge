e3cfdc0415c3460ca86ffe4eb2d149222c220e0a
Lawrence Murray <lawrence.murray@csiro.au>
2013-12-19 15:18:44 +0800

    Tidied up config files.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # generate data sets
-libbi sample @config.conf @prepare_obs.conf
+libbi sample @config.conf @prepare_obs.conf # test data sets
 octave --path oct -q --eval "prepare_obs;" # preset data set
 
 # fit bridge weight function
--- run.sh
+++ run.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-libbi sample @config.conf @posterior.conf
-
-for f in bridge bootstrap
-do
-  echo -n $f
-
-  libbi filter @config.conf @filter.conf --filter $f --obs-np 0 --output-file results/$f.nc.0 --noutputs 0
-  echo -n " 0"
-
-  # avoid parsing, code generation and building overhead on next runs
-  for i in {1..99}
-  do
-    echo -n " $i"
-    libbi filter @config.conf @filter.conf --filter $f --obs-np $i --output-file results/$f.nc.$i --noutputs 0 --dry-parse --dry-gen --dry-build
-  done
-  echo
-done
+libbi sample @config.conf @posterior.conf --filter bootstrap --output-file results/posterior_bootstrap.nc
+libbi sample @config.conf @posterior.conf --filter bridge --output-file results/posterior_bridge.nc
