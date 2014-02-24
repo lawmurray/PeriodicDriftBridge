c6fe629c8531191027c5f69e2211eb3c24c17fe8
Lawrence Murray <lawrence.murray@csiro.au>
2014-02-24 12:35:27 +0800

    Added plot_ess function and .gitignore file.

---
+++ oct/plot_ess.m
@@ -0,0 +1,25 @@
+function plot_ess(rep)
+    if nargin == 0
+        rep = 0;
+    end
+    
+    y = [];
+    filters = {'Bootstrap'; 'Bridge'};
+    for i = 1:length(filters)
+        file = sprintf('results/test_%s-%d.nc', tolower(filters{i}), rep);
+        nc = netcdf(file, 'r');
+        P = nc{'P'}(:);
+        L = nc{'loglikelihood'}(:,:)';
+        metric = ess(L)'./P;
+        y = [ y metric ];
+        ncclose(nc);
+    end
+
+
+    % plot bars
+    h = bar (log2(P), y);
+    for i = 1:length(h)
+        set (h(i), 'facecolor', fade(watercolour(i), 0.5));
+        set (h(i), 'edgecolor', watercolour(i));
+    end
+end
