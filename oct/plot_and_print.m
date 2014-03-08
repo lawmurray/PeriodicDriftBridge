b0e042a35a0171d4f1e90b1263f00ca03fa85e9e
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-08 10:16:31 -0800

    Updated plots to scatter style, bootstrap vs bridge, with ESS/t and CAR/t metrics.

--- oct/plot_and_print.m
+++ oct/plot_and_print.m
@@ -10,47 +10,39 @@
 % @end deftypefn
 %
 function plot_and_print ()
-    figDir = 'figs';
+    FIG_DIR = 'figs';
+    sz = [ 11 4 ];
+    set (figure(1), 'papersize', sz);
+    set (figure(1), 'paperposition', [0 0 sz]);
 
-    % state estimates
-    set (figure(1), 'papersize', [9 8]);
-    set (figure(1), 'paperposition', [0 0 9 8]);
-    orient ('portrait');
-    subplot(3,2,1);
-    plot_metric(3);
-    ax = axis();
-    subplot(3,2,2);
+    subplot(1,3,1);
     plot_metric(4);
-    axis(ax);
-    
-    subplot(3,2,3);
+    title('\sigma/t');
+    xlabel('Bootstrap');
+    ylabel('Bridge');
+    subplot(1,3,2);
     plot_metric(1);
-    axis([ ax(1) ax(2) 0 1 ]);
-    subplot(3,2,4);
+    title('ESS/t');
+    xlabel('Bootstrap');
+    subplot(1,3,3);
     plot_metric(2);
-    axis([ ax(1) ax(2) 0 1 ]);
-    
-    subplot(3,2,5);
-    plot_metric(5);
-    subplot(3,2,6);
-    plot_metric(6);
-    
-    saveas (figure(1), sprintf('%s/metrics.svg', figDir));
-    saveas (figure(1), sprintf('%s/metrics.pdf', figDir));
+    title('CAR/t');
+    xlabel('Bootstrap');
+    file = sprintf('%s/metrics.pdf', FIG_DIR);
+    saveas(figure(1), file);
+    system(sprintf('pdfcrop %s %s', file, file));
 
     clf;
-    set (figure(1), 'papersize', [3.5 8]);
-    set (figure(1), 'paperposition', [0 0 3.5 8]);
-    orient ('landscape');
     plot_weight;
-    saveas (figure(1), sprintf('%s/weights.svg', figDir));
-    saveas (figure(1), sprintf('%s/weights.pdf', figDir));
+    file = sprintf('%s/weights.pdf', FIG_DIR);
+    saveas(figure(1), file);
+    system(sprintf('pdfcrop %s %s', file, file));
 
-    clf;
-    set (figure(1), 'papersize', [3.5 8]);
-    set (figure(1), 'paperposition', [0 0 3.5 8]);
-    orient ('landscape');
-    plot_paths;
-    saveas (figure(1), sprintf('%s/paths.svg', figDir));
-    saveas (figure(1), sprintf('%s/paths.pdf', figDir));
+    %clf;
+    %set (figure(1), 'papersize', [3.5 8]);
+    %set (figure(1), 'paperposition', [0 0 3.5 8]);
+    %orient ('landscape');
+    %plot_paths;
+    %saveas (figure(1), sprintf('%s/paths.svg', figDir));
+    %saveas (figure(1), sprintf('%s/paths.pdf', figDir));
 end
