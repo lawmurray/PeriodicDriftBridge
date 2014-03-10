7c4afb2c12b8c55ec2cc61818fa7bbffb2df1b1d
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-10 19:33:21 +0930

    Added filtering to run.sh.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+libbi filter @config.conf @filter.conf --filter bootstrap --output-file results/filter_bootstrap.nc
+libbi filter @config.conf @filter.conf --filter bridge --output-file results/filter_bridge.nc
+
 libbi sample @config.conf @posterior.conf --filter bootstrap --output-file results/posterior_bootstrap.nc
 libbi sample @config.conf @posterior.conf --filter bridge --output-file results/posterior_bridge.nc
