fa5fdb5c04fe85e22a8e30027d7e6bf18945c889
Lawrence Murray <lawrence.murray@csiro.au>
2014-03-20 12:47:34 +0800

    Updated plot_metric() to use new Octave netcdf package rather than octcdf package, as octcdf seems not to support int64 type in files.

--- oct/plot_and_print.m
+++ oct/plot_and_print.m
@@ -13,22 +13,22 @@ function plot_and_print ()
     FIG_DIR = 'figs';
     mkdir(FIG_DIR);
     
-    sz = [ 11 4 ];
+    sz = [ 9 4 ];
     set (figure(1), 'papersize', sz);
     set (figure(1), 'paperposition', [0 0 sz]);
 
     subplot(1,3,1);
     plot_metric(1);
-    title('-MSE(log z)/Mean(t)');
+    title('MSE(log z)^{-1} Mean(t)^{-1}');
     xlabel('Bootstrap');
     ylabel('Bridge');
     subplot(1,3,2);
     plot_metric(2);
-    title('ESS(z)/Mean(t)');
+    title('ESS(z) Mean(t)^{-1}');
     xlabel('Bootstrap');
     subplot(1,3,3);
     plot_metric(3);
-    title('CAR(z)/Mean(t)');
+    title('CAR(z) Mean(t)^{-1}');
     xlabel('Bootstrap');
     file = sprintf('%s/metrics.pdf', FIG_DIR);
     saveas(figure(1), file);
--- oct/plot_metric.m
+++ oct/plot_metric.m
@@ -1,52 +1,49 @@
 function plot_metric(metric)
+    pkg load netcdf;
     if nargin < 1
         metric = 1;
     end
     
-    P = {};
     y = {};
     filters = {'Bootstrap'; 'Bridge'};
     for i = 1:length(filters)
         y{i} = [];
-        P{i} = [];
         rep = 0;
         file = sprintf('results/test_%s-%d.nc', tolower(filters{i}), rep);
         while exist(file, 'file')
-            nc = netcdf(file, 'r');
-            L = nc{'loglikelihood'}(:,:)';
-            T = nc{'time'}(:,:)';
+            L = ncread(file, 'loglikelihood');
+            T = ncread(file, 'time');
+            P = ncread(file, 'P');
         
             if metric == 1
                 % read 'true' likelihood
                 truth_file = sprintf('results/test_exact-%d.nc', rep);
-                truth_nc = netcdf(truth_file, 'r');
-                ll = truth_nc{'loglikelihood'}(:);
-                ncclose(truth_nc);
+                ll = ncread(truth_file, 'loglikelihood');
                 
-                tmp = 1.0./mean((L - ll).^2)'./mean(T)';
+                tmp = 1.0./mean((L - ll).^2)./mean(T);
             elseif metric == 2
-                tmp = ess(L)'./mean(T)';
+                tmp = ess(L)./mean(T);
             elseif metric == 3
-                tmp = car(L)'./mean(T)';
+                tmp = car(L)./mean(T);
             end
             y{i} = [ y{i}; tmp ];
-            P{i} = [ P{i}; nc{'P'}(:) ];
-            ncclose(nc);
             
             rep = rep + 1;
             file = sprintf('results/test_%s-%d.nc', tolower(filters{i}), rep);
         end
     end
 
-    for i=1:rows(y{1})
-        if y{1}(i) > y{2}(i)
-            col = watercolour(1);
-        else
-            col = watercolour(2);
+    for i = 1:rows(y{1})
+        for j = 1:columns(y{1})
+            if y{1}(i,j) > y{2}(i,j)
+                col = watercolour(1);
+            else
+                col = watercolour(2);
+            end
+            h = loglog(y{1}(i,j), y{2}(i,j), '.', 'color', col, ...
+                'markersize', sqrt(3*P(j)/pi));
+            hold on;
         end
-        h = loglog(y{1}(i), y{2}(i), 'o', 'color', col, 'markersize', ...
-                   sqrt(P{1}(i)/pi), 'linewidth', 3);
-        hold on;
     end
 
     %axis('tight');
@@ -56,5 +53,6 @@ function plot_metric(metric)
     loglog([mn mx], [mn mx], '--k');
     axis([mn mx mn mx]);
     axis('square');
-    grid minor on;
+    grid on;
+    grid minor off;
 end
